3542cf464ba5029241a012438d5727f23b34def8
Florestan Lefevre <florestanlefevre@gmail.com>
2021-03-13 11:45:32 -0800

    Add media queries for use

---
+++ src/shared/MediaQueries.js
@@ -0,0 +1,8 @@
+const QUERY_FOR_PHONE_ONLY = `(max-width: 599px)`;
+export const MQ_FOR_PHONE_ONLY = `@media ${QUERY_FOR_PHONE_ONLY}`;
+
+const QUERY_FOR_TABLET_ONLY = `(min-width: 600px) and (max-width: 899px)`;
+export const MQ_FOR_TABLET_ONLY = `@media ${QUERY_FOR_TABLET_ONLY}`;
+
+const QUERY_FOR_DESKTOP_ONLY = `(min-width: 900px)`;
+export const MQ_FOR_DESKTOP_ONLY = `@media ${QUERY_FOR_DESKTOP_ONLY}`;
